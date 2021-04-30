321809f7c901c6dd1e91f334aad5a67075b864f1
Joseph Koshakow <koshy44@gmail.com>
2021-04-30 13:28:48 -0400

    Implement abort functionality

--- src/include/transaction.h
+++ src/include/transaction.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <atomic>
 #include "eager_version_manager.h"
 #include "lazy_version_manager.h"
 
 // If LAZY_VERSIONING is set to 0 then eager versioning will be used
 #define LAZY_VERSIONING 1
+// If PESSIMISTIC_CONFLICT_DETECTION is set to 0 then optimistic conflict detection will be used
+#define PESSIMISTIC_CONFLICT_DETECTION 1
 
 class Transaction {
 
@@ -22,6 +25,9 @@ public:
      */
     template<typename T>
     void Store(T *address, T value) {
+        if(state_ == ABORTED) {
+            Abort();
+        }
 #if LAZY_VERSIONING
         lazy_version_manager_.Store(address, value);
 #else
@@ -42,6 +48,9 @@ public:
      */
     template<typename T>
     T Load(T *address) {
+        if(state_ == ABORTED) {
+            Abort();
+        }
 #if LAZY_VERSIONING
         T res;
         // Check if write is in write buffer
@@ -73,6 +82,13 @@ public:
      */
     uint64_t GetTransactionId();
 
+    /**
+     * Mark that this transaction has been aborted
+     *
+     * @return true if the transaction was successfully aborted false otherwise
+     */
+    bool MarkAborted();
+
 private:
     uint64_t transaction_id_;
 #if LAZY_VERSIONING
@@ -80,4 +96,17 @@ private:
 #else
     EagerVersionManager eager_version_manager_;
 #endif
+    /**
+     * Indicates the state of a transaction.
+     * 0 - running
+     * 1 - committing
+     * 2 - aborting
+     * TODO try and implement with enum
+     */
+    std::atomic<int> state_;
+    std::condition_variable abort_cv_;
+
+    static constexpr int RUNNING = 0;
+    static constexpr int COMMITTING = 1;
+    static constexpr int ABORTED = 2;
 };
--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "include/transaction.h"
+#include "include/abort_exception.h"
 
-Transaction::Transaction(uint64_t transaction_id) : transaction_id_(transaction_id) {}
+Transaction::Transaction(uint64_t transaction_id) : transaction_id_(transaction_id), aborted_(false), state_(0) {}
 
 void Transaction::Abort() {
 #if LAZY_VERSIONING
@@ -8,20 +10,42 @@ void Transaction::Abort() {
 #else
     eager_version_manager_.Abort();
 #endif
+    abort_cv_.notify_all();
+    throw AbortException("Transaction aborted");
 }
 
 void Transaction::XEnd() {
+    int cur_val = RUNNING;
+    bool exchanged = state_.compare_exchange_strong(cur_val, COMMITTING);
+    if(!exchanged && cur_val == ABORTED) {
+        Abort();
+    } else if(!exchanged && cur_val == COMMITTING) {
+        std::cerr << "Tried to commit an already committing transaction" << std::endl;
+    } else if (exchanged) {
 #if LAZY_VERSIONING
-    auto &write_buffer = lazy_version_manager_.GetWriteBuffer();
-    for (const auto &write : write_buffer) {
-        std::memcpy(write.first, write.second.data_, write.second.size_);
-    }
-    lazy_version_manager_.XEnd();
+        auto &write_buffer = lazy_version_manager_.GetWriteBuffer();
+        for (const auto &write : write_buffer) {
+            std::memcpy(write.first, write.second.data_, write.second.size_);
+        }
+        lazy_version_manager_.XEnd();
 #else
-    eager_version_manager_.XEnd();
+        eager_version_manager_.XEnd();
 #endif
+    }
 }
 
 uint64_t Transaction::GetTransactionId() {
     return transaction_id_;
+}
+
+bool Transaction::MarkAborted() {
+    int cur_val = RUNNING;
+    bool exchanged = state_.compare_exchange_strong(cur_val, ABORTED);
+    if(exchanged || cur_val == ABORTED) {
+        // Mutex is sort of pointless but just needed for cv API
+        std::mutex mutex;
+        std::unique_lock<std::mutex> lock(mutex);
+        abort_cv_.wait(lock);
+    }
+    return exchanged || cur_val == ABORTED;
 }
